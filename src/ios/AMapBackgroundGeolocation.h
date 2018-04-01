26b61287fb8d5d3f0c733efc36bc3f0da5b99112
yanglongji <yanglongji9@gmail.com>
2018-04-01 12:45:50 +0800

    add ios plugin

---
+++ src/ios/AMapBackgroundGeolocation.h
@@ -0,0 +1,16 @@
+//
+//  Lyxpay.h
+//  PluginTest
+//
+//  Created by 冬追夏赶 on 9/15/16.
+//
+//
+
+#import <Cordova/CDVPlugin.h>
+
+@interface AMapBackgroundGeolocation : CDVPlugin
+
+- (void)start:(CDVInvokedUrlCommand*)command;
+- (void)stop:(CDVInvokedUrlCommand*)command;
+
+@end
---
+++ src/ios/AMapBackgroundGeolocation.m
@@ -0,0 +1,58 @@
+//
+// Lyxpay.m
+// PluginTest
+//
+// Created by 冬追夏赶 on 9 / 15 / 16.
+//
+//
+
+#import "AMapBackgroundGeolocation.h"
+#import <AMapLocationKit/AMapLocationKit.h>
+
+
+@interface AMapBackgroundGeolocation () <AMapLocationManagerDelegate>
+@property (nonatomic, strong) CDVInvokedUrlCommand *tempCommand;
+@property (nonatomic, strong) AMapLocationManager *locationManager;
+// @property(nonatomic,strong)NSString *apikey;
+@end
+
+@implementation AMapBackgroundGeolocation
+
+-(void)pluginInitialize
+{
+    // self.apikey = [[self.commandDelegate settings] objectForKey:@"APIKEY"];
+    [AMapServices sharedServices].apiKey = (NSString *)[[self.commandDelegate settings] objectForKey:@"APIKEY"];
+    self.locationManager = [[AMapLocationManager alloc] init];
+    
+    [self.locationManager setDelegate:self];
+    
+    //设置不允许系统暂停定位
+    [self.locationManager setPausesLocationUpdatesAutomatically:NO];
+    
+    //设置允许在后台定位
+    [self.locationManager setAllowsBackgroundLocationUpdates:YES];
+    
+    //开启带逆地理连续定位
+    [self.locationManager setLocatingWithReGeocode:YES];
+}
+
+- (void)start : (CDVInvokedUrlCommand *)command
+{
+    self.tempCommand = command;
+    [self.locationManager startUpdatingLocation];
+}
+
+- (void)stop : (CDVInvokedUrlCommand *)command
+{
+    self.tempCommand = command;
+    [self.locationManager stopUpdatingLocation];
+}
+
+
+- (void)sendResp:(NSDictionary *)resultDic{
+    CDVPluginResult* pluginResult = nil;
+    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:self.tempCommand.callbackId];
+}
+
+@end
