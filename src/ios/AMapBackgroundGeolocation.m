839079c9df46b811f63849cc4a120e8a479689da
yanglongji <yanglongji9@gmail.com>
2018-04-25 23:35:50 +0800

    complete ios

--- src/ios/AMapBackgroundGeolocation.m
+++ src/ios/AMapBackgroundGeolocation.m
@@ -14,7 +14,7 @@
 @interface AMapBackgroundGeolocation () <AMapLocationManagerDelegate>
 @property (nonatomic, strong) CDVInvokedUrlCommand *tempCommand;
 @property (nonatomic, strong) AMapLocationManager *locationManager;
-// @property(nonatomic,strong)NSString *apikey;
+@property (nonatomic, strong) NSDate *timestamp;
 @end
 
 @implementation AMapBackgroundGeolocation
@@ -29,19 +29,21 @@
     [self.locationManager setDelegate:self];
     
     //设置不允许系统暂停定位
-    [self.locationManager setPausesLocationUpdatesAutomatically:NO];
+//    [self.locationManager setPausesLocationUpdatesAutomatically:NO];
     
     //设置允许在后台定位
-    [self.locationManager setAllowsBackgroundLocationUpdates:YES];
+//    [self.locationManager setAllowsBackgroundLocationUpdates:YES];
     
     //开启带逆地理连续定位
     [self.locationManager setLocatingWithReGeocode:YES];
+    
+//    timestamp =
 }
 
 - (void)start : (CDVInvokedUrlCommand *)command
 {
     self.tempCommand = command;
-    self.locationManager.distanceFilter = 100;
+    self.locationManager.distanceFilter = 1;
     [self.locationManager startUpdatingLocation];
 }
 
@@ -52,9 +54,9 @@
 }
 
 
-- (void)sendResp:(NSDictionary *)resultDic{
+- (void)sendResp:(NSDictionary *)resultDic status:(CDVCommandStatus)status{
     CDVPluginResult* pluginResult = nil;
-    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+    pluginResult = [CDVPluginResult resultWithStatus:status];
     [self.commandDelegate sendPluginResult:pluginResult callbackId:self.tempCommand.callbackId];
 }
 
@@ -62,13 +64,21 @@
 
 - (void)amapLocationManager:(AMapLocationManager *)manager didFailWithError:(NSError *)error
 {
-    NSLog(@"%s, amapLocationManager = %@, error = %@", __func__, [manager class], error);
+    //    NSLog(@"%@", error);
+    NSDictionary *dic = @{@"errorCode":[NSString stringWithFormat:@"%ld",[error code]]};
+//    NSLog(@"%@", dic);
+    [self sendResp:dic status:CDVCommandStatus_ERROR];
 }
 
 - (void)amapLocationManager:(AMapLocationManager *)manager didUpdateLocation:(CLLocation *)location reGeocode:(AMapLocationReGeocode *)reGeocode
 {
-    NSLog(@"location:{lat:%f; lon:%f; accuracy:%f}", location.coordinate.latitude, location.coordinate.longitude, location.horizontalAccuracy);
-
+    // reGeocode  =  AMapLocationReGeocode:{formattedAddress:浙江省杭州市西湖区金蓬街758舞蹈艺术中心(金地自在城教学点); country:中国;province:浙江省; city:杭州市; district:西湖区; citycode:0571; adcode:330106; street:金蓬街; number:293号; POIName:758舞蹈艺术中心(金地自在城教学点); AOIName:(null);}
+    if([self timestamp] == nil || [[self timestamp] timeIntervalSince1970] - [location.timestamp timeIntervalSince1970] > 600){ //10分钟回调1次
+        self.timestamp = location.timestamp;
+        NSDictionary *dic = @{@"lng":[NSString stringWithFormat:@"%f",location.coordinate.longitude],@"lat":[NSString stringWithFormat:@"%f",location.coordinate.latitude],@"address":[NSString stringWithFormat:@"%@",reGeocode.formattedAddress]};
+//        NSLog(@"%@", dic);
+        [self sendResp:dic status:CDVCommandStatus_OK];
+    }
 }
 
 @end
